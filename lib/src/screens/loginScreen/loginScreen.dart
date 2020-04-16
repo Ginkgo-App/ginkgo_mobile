5e4c6447f1ae3a06381c47766688a3b82315281d
Hieren <hienlh1298@gmail.com>
2020-04-16 22:28:07 +0700

    [#1] Refactor code in login screen

--- lib/src/screens/loginScreen/loginScreen.dart
+++ lib/src/screens/loginScreen/loginScreen.dart
@@ -1,6 +1,11 @@
 part of screens;
 
-class LoginScreen extends StatelessWidget {
+class LoginScreen extends StatefulWidget {
+  @override
+  _LoginScreenState createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
   _onLogin() {}
 
   _onForgetPassword() {}
@@ -24,108 +29,11 @@ class LoginScreen extends StatelessWidget {
               mainAxisAlignment: MainAxisAlignment.center,
               crossAxisAlignment: CrossAxisAlignment.stretch,
               children: <Widget>[
-                const SizedBox(height: 30),
-                LogoWidget(
-                  width: MediaQuery.of(context).size.width / 2,
-                  height: MediaQuery.of(context).size.width / 2,
-                ),
-                const SizedBox(height: 20),
-                TextFormField(
-                  decoration: InputDecoration(
-                    labelText: Strings.common.email,
-                    border: GradientUnderlineInputBorder(
-                      focusedGradient:
-                          GradientColor.of(context).primaryGradient,
-                    ),
-                  ),
-                ),
-                const SizedBox(height: 20),
-                TextFormField(
-                  obscureText: true,
-                  decoration: InputDecoration(
-                      labelText: Strings.common.password,
-                      border: GradientUnderlineInputBorder(
-                        focusedGradient:
-                            GradientColor.of(context).primaryGradient,
-                      )),
-                ),
-                const SizedBox(height: 40),
-                PrimaryButton(
-                  title: Strings.button.login,
-                  onPressed: _onLogin,
-                ),
-                const SizedBox(height: 10),
-                GestureDetector(
-                  onTap: _onForgetPassword,
-                  child: Text(
-                    Strings.button.forgotPassword,
-                    textAlign: TextAlign.center,
-                    style: context.textTheme.body1.apply(
-                      color: Colors.blueAccent,
-                    ),
-                  ),
-                ),
+                ..._buildFormAndLogo(),
                 Expanded(child: const SizedBox.shrink()),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: <Widget>[
-                    Text(
-                      '                    ',
-                      style: TextStyle(
-                        decoration: TextDecoration.lineThrough,
-                        color: context.colorScheme.onSurface,
-                      ),
-                    ),
-                    Text(
-                      Strings.loginScreen.orLoginWith,
-                      style: context.textTheme.body1.copyWith(
-                          color: context.colorScheme.onBackground,
-                          fontStyle: FontStyle.italic),
-                    ),
-                    Text(
-                      '                    ',
-                      style: TextStyle(
-                        decoration: TextDecoration.lineThrough,
-                        color: context.colorScheme.onSurface,
-                      ),
-                    ),
-                  ],
-                ),
-                const SizedBox(height: 10),
-                Wrap(
-                  alignment: WrapAlignment.center,
-                  spacing: 5,
-                  children: <Widget>[
-                    CupertinoButton(
-                      padding: EdgeInsets.zero,
-                      onPressed: _onFacebookLogin,
-                      child: Image.asset('assets/icons/facebook.png'),
-                    ),
-                    CupertinoButton(
-                      padding: EdgeInsets.zero,
-                      child: Image.asset('assets/icons/google-plus.png'),
-                      onPressed: _onGoogleLogin,
-                    ),
-                  ],
-                ),
+                ..._buildSocialLogin(),
                 const SizedBox(height: 20),
-                Text(
-                  Strings.loginScreen.doNotHaveAccount,
-                  textAlign: TextAlign.center,
-                  style: context.textTheme.body1
-                      .apply(color: context.colorScheme.onBackground),
-                ),
-                GestureDetector(
-                  onTap: _onRegister,
-                  child: Text(
-                    Strings.loginScreen.registerNow,
-                    textAlign: TextAlign.center,
-                    style: context.textTheme.body1.copyWith(
-                      color: Colors.blueAccent,
-                      decoration: TextDecoration.underline,
-                    ),
-                  ),
-                ),
+                ..._buildBottom(),
                 const SizedBox(height: 20),
               ],
             ),
@@ -133,5 +41,118 @@ class LoginScreen extends StatelessWidget {
         ),
       ),
     );
+  }
+
+  _buildFormAndLogo() {
+    return [
+      const SizedBox(height: 30),
+      LogoWidget(
+        width: MediaQuery.of(context).size.width / 2,
+        height: MediaQuery.of(context).size.width / 2,
+      ),
+      const SizedBox(height: 20),
+      TextFormField(
+        decoration: InputDecoration(
+          labelText: Strings.common.email,
+          border: GradientUnderlineInputBorder(
+            focusedGradient: GradientColor.of(context).primaryGradient,
+          ),
+        ),
+      ),
+      const SizedBox(height: 20),
+      TextFormField(
+        obscureText: true,
+        decoration: InputDecoration(
+            labelText: Strings.common.password,
+            border: GradientUnderlineInputBorder(
+              focusedGradient: GradientColor.of(context).primaryGradient,
+            )),
+      ),
+      const SizedBox(height: 40),
+      PrimaryButton(
+        title: Strings.button.login,
+        onPressed: _onLogin,
+      ),
+      const SizedBox(height: 10),
+      GestureDetector(
+        onTap: _onForgetPassword,
+        child: Text(
+          Strings.button.forgotPassword,
+          textAlign: TextAlign.center,
+          style: context.textTheme.body1.apply(
+            color: Colors.blueAccent,
+          ),
+        ),
+      ),
+    ];
+  }
+
+  _buildSocialLogin() {
+    return [
+      Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Text(
+            '                    ',
+            style: TextStyle(
+              decoration: TextDecoration.lineThrough,
+              color: context.colorScheme.onSurface,
+            ),
+          ),
+          Text(
+            Strings.loginScreen.orLoginWith,
+            style: context.textTheme.body1.copyWith(
+                color: context.colorScheme.onBackground,
+                fontStyle: FontStyle.italic),
+          ),
+          Text(
+            '                    ',
+            style: TextStyle(
+              decoration: TextDecoration.lineThrough,
+              color: context.colorScheme.onSurface,
+            ),
+          ),
+        ],
+      ),
+      const SizedBox(height: 10),
+      Wrap(
+        alignment: WrapAlignment.center,
+        spacing: 5,
+        children: <Widget>[
+          CupertinoButton(
+            padding: EdgeInsets.zero,
+            onPressed: _onFacebookLogin,
+            child: Image.asset('assets/icons/facebook.png'),
+          ),
+          CupertinoButton(
+            padding: EdgeInsets.zero,
+            child: Image.asset('assets/icons/google-plus.png'),
+            onPressed: _onGoogleLogin,
+          ),
+        ],
+      ),
+    ];
+  }
+
+  _buildBottom() {
+    return [
+      Text(
+        Strings.loginScreen.doNotHaveAccount,
+        textAlign: TextAlign.center,
+        style: context.textTheme.body1
+            .apply(color: context.colorScheme.onBackground),
+      ),
+      GestureDetector(
+        onTap: _onRegister,
+        child: Text(
+          Strings.loginScreen.registerNow,
+          textAlign: TextAlign.center,
+          style: context.textTheme.body1.copyWith(
+            color: Colors.blueAccent,
+            decoration: TextDecoration.underline,
+          ),
+        ),
+      ),
+    ];
   }
 }
